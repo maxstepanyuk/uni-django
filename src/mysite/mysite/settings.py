532321a32c21dbebaa1082ae023ea06b865da24f
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2025-05-11 23:17:32 +0300

    changed db to pSQL

--- src/mysite/mysite/settings.py
+++ src/mysite/mysite/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'temp_uni_django_mysite_db',
+        'HOST': '',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
