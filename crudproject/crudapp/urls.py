b9df305bbc1aea99c88db1106ce86e55504f2c60
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2025-05-12 02:21:05 +0300

    added root redorect to sv

--- crudproject/crudapp/urls.py
+++ crudproject/crudapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/sv/', permanent=False)),
+    
     path('ofv/', views.orderFormView, name='order_url'),
     path('sv/', views.showView, name='show_url'),
     path('up/<int:f_oid>', views.updateView, name= 'update_url'),
